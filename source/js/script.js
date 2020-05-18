175c7af63eebd6103b66d0df1e0ef31f284647c8
MargaritaDzhubaba <rikajubaba@gmail.com>
2020-05-19 00:59:23 +0300

    Добавляет JS для валидации формы отзыва

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,19 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = navMain.querySelector('.main-nav__toggle');
+
+var form = document.querySelector('.form');
+var userName = form.querySelector("[name=user-name]");
+var userSurname = form.querySelector("[name=user-surname]");
+var userPhone = form.querySelector("[name=phone]");
+var userEmail = form.querySelector("[name=email]");
+
+var buttonSubmit = form.querySelector('.form__button');
+var modalSuccess = document.querySelector('.form-modal__success');
+var modalSuccessBtn = modalSuccess.querySelector('#button-success');
+var modalError = document.querySelector('.form-modal__error');
+var modalErrorBtn = modalError.querySelector('#button-error');
+
+// navigation
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -10,5 +24,38 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+// validation
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if ((!userName.value || !userSurname.value || !userPhone.value || !userEmail.value)) {
+    modalError.classList.add("modal--opened");
+  } else {
+    modalSuccess.classList.add("modal--opened");
+  }
+});
+
+modalSuccessBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalSuccess.classList.remove("modal--opened");
+});
+
+modalErrorBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalError.classList.remove("modal--opened");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalError.classList.contains("modal--opened")) {
+      modalError.classList.remove("modal--opened");
+    } else
+    if (modalSuccess.classList.contains("modal--opened")) {
+      modalSuccess.classList.remove("modal--opened");
+    }
   }
 });
